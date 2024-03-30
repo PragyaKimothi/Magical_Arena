97ddc6c732661865ccfdaec5a9918df7c55e1f1a
PragyaKimothi <pragyakimothi@gmail.com>
2024-03-30 18:51:48 +0530

    craeting two Players

--- MagicalArena.java
+++ MagicalArena.java
@@ -35,6 +35,9 @@ public Player(int health, int strength, int attack) {                           
     }
     public class MagicalArena {
         public static void main(String[] args) {
+            // creating two players
+            Player playerA = new Player(50, 5, 10);
+            Player playerB = new Player(100, 10, 5);
             
            
     }
