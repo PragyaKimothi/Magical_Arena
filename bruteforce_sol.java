19dc9136f93ec3970e66fcccb8f7abd7f615e497
PragyaKimothi <pragyakimothi@gmail.com>
2024-03-30 16:25:25 +0530

    Player Creation and Dice Creation

--- bruteforce_sol.java
+++ bruteforce_sol.java
@@ -4,7 +4,11 @@ public class Player {
     private int health;
     private int strength;
     private int attack;
-
-
-
+}
 // Dice Creation
+public class Dice {
+    private int sides;
+    public int roll() {
+        // Generate a random value ranging between 1-6.
+    }
+}
