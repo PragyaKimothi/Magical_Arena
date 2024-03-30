ec1b1f3ff6182069798c59c61502d0739a022f88
PragyaKimothi <pragyakimothi@gmail.com>
2024-03-30 16:31:52 +0530

    Player Creation and Dice Creation

--- bruteforce_sol.java
+++ bruteforce_sol.java
@@ -1,14 +1,23 @@
 //Brute Force Solution
-//Player Creation
+//Creation the creating and its attributes
 public class Player {
-    private int health;
-    private int strength;
-    private int attack;
+    private int health;                   //attributes of a Player
+    private int strength;                 //attributes of a Player
+    private int attack;                   //attributes of a Player
 }
-// Dice Creation
+//Creating The Dice
 public class Dice {
-    private int sides;
-    public int roll() {
+    private int side;
+    public int dieroll() {
         // Generate a random value ranging between 1-6.
+    }
+}
+//Creating The Game
+public class Game {
+    public void startMatch(Player player1, Player player2) {
+        // Determine attacking and defending order based on health
+        // Loop until one player's health reaches 0
+        // Alternate turns between players, rolling dice and executing attacks
+        // Display match results
     }
 }
