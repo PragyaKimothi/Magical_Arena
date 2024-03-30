0ac571092c853b11255a014301e41e6cda09c6ad
PragyaKimothi <pragyakimothi@gmail.com>
2024-03-30 17:15:50 +0530

    Problem Solution

--- MagicalArena.java
+++ MagicalArena.java
@@ -6,6 +6,12 @@ class Player {
     private int attack;
     private Random random;
 
+public Player(int health, int strength, int attack) {
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+        this.random = new Random();
+    }
     
     }
 
