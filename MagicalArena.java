d5961ecee0b5b12d537170cec646c97f428a65e0
PragyaKimothi <pragyakimothi@gmail.com>
2024-03-30 17:21:37 +0530

    Problem Solution

--- MagicalArena.java
+++ MagicalArena.java
@@ -12,6 +12,20 @@ public Player(int health, int strength, int attack) {
         this.attack = attack;
         this.random = new Random();
     }
+    public int getHealth() {
+        return health;
+    }
+
+    public void attack(Player opponent) {
+        int attackRoll = random.nextInt(6) + 1; // Roll attacking dice
+        int defenseRoll = random.nextInt(6) + 1; // Roll defending dice
+
+        int attackDamage = attack * attackRoll;
+        int defenseDamage = opponent.strength * defenseRoll;
+
+        int damageDealt = Math.max(0, attackDamage - defenseDamage);
+        opponent.health -= damageDealt;
+    }
     
     }
 
