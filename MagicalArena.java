104d52c430d888d768e1f1ac83fc467c9199c428
PragyaKimothi <pragyakimothi@gmail.com>
2024-03-30 20:39:28 +0530

    importing scanner class

--- MagicalArena.java
+++ MagicalArena.java
@@ -1,12 +1,51 @@
-//import java.util.Random;
+import java.util.Random;
+import java.util.Scanner;
+//Creating Player and its atrributes.
+class Player {                                                     
+    private int health;
+    private int strength;
+    private int attack;
+    private Random random;
 
+    public Player(int health, int strength, int attack) {                        //attributes of the player
+        this.health = health;
+        this.strength = strength;
+        this.attack = attack;
+        this.random = new Random();
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void attack(Player Competer) {
+        int attackDieRoll = rollDie();                         // Attacking Player rolls the attacking dice
+        int defenseDieRoll = Competer.rollDie();               // Defending Player rolls the defending dice
+
+        //Calculating the attack damage created by the attacking player 
+        int attackDamage = attack * attackDieRoll;
+
+        //Calculating the damage defended by the defending player
+        int defenseDamage = Competer.strength * defenseDieRoll;
+
+        //Calculating the damage 
+        int damageDealt = Math.max(0, attackDamage - defenseDamage);
+
+        //Reducing the strength of the Defender
+        Competer.health -= damageDealt;
+    }
+
+    //Creating the Die to roll
+    private int rollDie() {
+        return random.nextInt(6)+1;            //Die with 6 sides having values ranging from 1 to 6.
+    }
+}
     
-    //Creating the MagicalArena class
+//Creating the MagicalArena class
 public class MagicalArena {
-        public static void main(String[] args) {
             // creating two players 
             Player playerA = new Player(50, 5, 10);
-            Player playerB = new Player(100, 10, 5);
+            Player playerB = new Player(100,10,5);
             
             //Checking the health of the players and comparing them to chooose who gets to attack first.
             //Player with lower  health attacks first. 
